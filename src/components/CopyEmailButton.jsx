7de7411414f4726a63d8fdd13b7ebdf45797f9bd
dhruvwadhwani7 <dhruvwadhwani77277@gmail.com>
2025-06-25 12:18:22 +0530

    Final

--- src/components/CopyEmailButton.jsx
+++ src/components/CopyEmailButton.jsx
@@ -20,12 +20,12 @@ const CopyEmailButton = () => {
 
     if (isIOS) {
       // Open in a new tab for iOS devices
-      window.open("/assets/DhruvWadhwani_Resume.pdf", "_blank");
+      window.open("/assets/Dhruv_Wadhwani_Resume.pdf", "_blank");
     } else {
       // Direct download for desktop and Android
       const link = document.createElement("a");
       link.href = "/assets/DhruvWadhwani_Resume.pdf";
-      link.download = "DhruvWadhwani_Resume.pdf";
+      link.download = "Dhruv_Wadhwani_Resume.pdf";
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
